44b2fb518a144e1e33e4717f3f9bf2d266cd7eaa
Ashmit0405 <173537273+Ashmit0405@users.noreply.github.com>
2025-02-01 02:15:25 +0530

    Some changes

--- flask_api/app.py
+++ flask_api/app.py
@@ -89,4 +89,4 @@ def recommend():
 if __name__ == '__main__':
     host = os.getenv('HOST', '0.0.0.0')
     port = int(os.getenv('PORT', 5000))
-    app.run(host=host, port=5000,debug=True)
+    app.run(host=host, port=port,debug=True)
