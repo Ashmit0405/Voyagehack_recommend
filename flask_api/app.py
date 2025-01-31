7b6307cec938561357d147f84b8861cc16309089
Ashmit0405 <173537273+Ashmit0405@users.noreply.github.com>
2025-01-31 23:48:16 +0530

    Production changes

--- flask_api/app.py
+++ flask_api/app.py
@@ -101,5 +101,5 @@ def recommend():
 
 if __name__ == '__main__':
     host = os.getenv('HOST', '0.0.0.0')
-    # port = int(os.getenv('PORT', 5000))
+    port = int(os.getenv('PORT', 5000))
     app.run(host=host, port=5000)
